5adb778a40f2284b9ab375f685d78d484e38430e
gramirez59 <gramirez59@misena.edu.co>
2021-06-08 00:07:41 -0500

    Ajuste de Cors

--- Antojitos/Startup.cs
+++ Antojitos/Startup.cs
@@ -48,6 +48,12 @@ namespace Antojitos
 
             app.UseRouting();
 
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true)
+                .AllowCredentials());
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
